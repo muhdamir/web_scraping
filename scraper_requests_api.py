a6bf56a0c1a578f38875d2f16ab8e4eb48729316
muhdamir <62650104+muhdamir@users.noreply.github.com>
2023-12-31 08:45:26 +0800

    Update scraper_requests_api.py
    add logger

--- scraper_requests_api.py
+++ scraper_requests_api.py
@@ -12,9 +12,11 @@ we can inspect the API call in our browser.
 
 """
 
+import logging
+import os
 import pickle
 from datetime import MAXYEAR, MINYEAR
-from typing import Annotated
+from typing import Annotated, Literal
 
 import pandas as pd
 import requests
@@ -26,6 +28,14 @@ from pydantic import (
     field_serializer,
     model_serializer,
 )
+
+# setup logger
+logging.basicConfig(
+    format="{asctime} - {levelname}: {name}.{funcName} - {message}",
+    style="{",
+)
+logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.INFO)
 
 
 class CustomBaseModel(BaseModel):
@@ -94,27 +104,55 @@ class ResponseModel(BaseModel):
     data: list[PostTypeModel]
 
 
-query: dict[str, int | str] = {
-    "category": 1020,
-    "from": 0,
-    "include": "extra_images,body",
-    "limit": 200,
-    "type": "sell",
-}
-res = requests.get(
-    "https://search.mudah.my/v1/search",
-    params=query,
-    timeout=30,
-)
+def main(
+    file_type: Literal["pickle", "csv"] = "csv",
+):
+    logger.info("✅  application started")
 
-data = ResponseModel.model_validate_json(res.text)
-dict_data = data.model_dump(by_alias=True)
+    query: dict[str, str | int] = {
+        "category": 1020,
+        "from": 0,
+        "include": "extra_images,body",
+        "limit": 200,
+        "type": "sell",
+    }
+    logger.info("⚒️  requesting to mudah.my's API...")
+    res = requests.get(
+        "https://search.mudah.my/v1/search",
+        params=query,
+        timeout=30,
+    )
+    if res.status_code != 200:
+        logger.error("❌  get request failed")
+        return
 
-# save tabular data to csv
-df = pd.DataFrame(dict_data["data"])
-df.to_csv("data.csv", index=False)
+    logger.info("✅  get request is successfull")
+    data_preview = f"🔎  preview of the data: {res.text[:35]}..."
+    logger.info(data_preview)
+
+    data = ResponseModel.model_validate_json(res.text)
+    dict_data = data.model_dump(by_alias=True)
+
+    match file_type:
+        case "csv":
+            # save tabular data to csv
+            logger.info("⚒️  saving to a csv file")
+            df = pd.DataFrame(dict_data["data"])
+            df.to_csv("data.csv", index=False)
+            message = f"✅  data is saved in {os.path.join(os.getcwd(), 'data.csv')}"
+            logger.info(message)
+
+        case "pickle":
+            # save dict data to pickle
+            logger.info("⚒️  saving to a pickle file")
+            with open(file="data.pkl", mode="+wb") as file:
+                pickle.dump(dict_data, file=file)
+            message = f"✅  data is saved in {os.path.join(os.getcwd(), 'data.pkl')}"
+            logger.info(message)
+
+        case _:
+            logger.warning("⚠️  wrong argument. Data is not saved")
 
 
-# save dict data to pickle
-with open(file="data.pkl", mode="+wb") as file:
-    pickle.dump(dict_data, file=file)
+if __name__ == "__main__":
+    main(file_type="csv")
